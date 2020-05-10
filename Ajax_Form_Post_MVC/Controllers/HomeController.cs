6a025032e35cdd85c8d850fc727b9e4c06185bca
Wahid Ahemd <wahidahmed1992@outlook.com>
2020-05-10 11:57:09 +0600

    ajax save

---
+++ Ajax_Form_Post_MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Ajax_Form_Post_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ajax_Form_Post_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(PersonModel person)
+        {
+            
+            System.Threading.Thread.Sleep(1000);
+            try
+            {
+                int personId = Convert.ToInt32(person.PersonId);
+                string name = person.Name;
+                string gender = person.Gender;
+                string city = person.City;
+                return Json(person);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+           
+        }
+    }
+}
